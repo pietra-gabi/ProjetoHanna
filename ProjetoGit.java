207dc3b2b4ca3d30ea16ff31161a54cc6f95d9fc
pietra-gabi <pietra.rodrigues@alunos.ifsuldeminas.edu.br>
2025-10-01 12:14:09 -0300

    Add files via upload

---
+++ ProjetoGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetogit;
+
+/**
+ *
+ * @author 16873757632
+ */
+public class ProjetoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Este é meu projeto Java com Git e GitHub.");
+    }
+    
+}
